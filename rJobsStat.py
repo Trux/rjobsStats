233dec50aa84f972699757fb9e9d766a5e9838a6
Truxs <clyraud@yahoo.fr>
2013-02-22 22:09:28 +0100

    added json output

--- rJobsStat.py
+++ rJobsStat.py
@@ -1,24 +1,40 @@
 #!usr/bin/python
 #-*-coding:utf-8-*-
-import requests, datetime
+import requests, collections, json
+from collections import defaultdict
 
 result=''
+companyTemp=[]
+companyList=[]
+companyRank = {}
 
 start = datetime.datetime.now()
-for i in range (0, 30):
-	print i
+for i in range (0,30):
 	page = requests.get('https://remixjobs.com/offre/emploi/page/'+str(i))
 	temp =page.text
 	result = result + temp
 
-print len(result)
-
+#get company name
 answer =[n for n in xrange(len(result)) if result.find('/company/', n) == n]
 
+#get rid of eveything before and after the company's name
 
-print result[answer[0]+9:answer[0]+40]
+for j in range(0,len(answer)):
+	companyTemp.append(result[answer[j]+9:answer[j]+40])
+
+for k in range(0,len(companyTemp)):
+	end = companyTemp[k].find('/')
+	companyList.append(companyTemp[k][0:end])
+companyList.sort()
+
+freq = collections.defaultdict(int)
+for company in companyList:
+    freq[company] += 1
+
+jsonOutput = json.dumps(freq)
 
 stop = datetime.datetime.now()
-#print when the soft started and ended
-print str(start)
-print str(stop)
+
+
+
+
