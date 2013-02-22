67fa04b8bb640f415a2510e10c47c964bd1f9464
Truxs <clyraud@yahoo.fr>
2013-02-22 20:15:52 +0100

    add finding of company

--- rJobsStat.py
+++ rJobsStat.py
@@ -1,9 +1,24 @@
 #!usr/bin/python
 #-*-coding:utf-8-*-
-import requests
+import requests, datetime
 
-page = requests.get('https://remixjobs.com/offre/emploi/at/Paris%2C%20France/lat/48.856614/lng/2.3522219000000177/dist/1000')
-result = page.text
-where = result.find('class="company"')
-print where
-print result[where:where+50]
+result=''
+
+start = datetime.datetime.now()
+for i in range (0, 30):
+	print i
+	page = requests.get('https://remixjobs.com/offre/emploi/page/'+str(i))
+	temp =page.text
+	result = result + temp
+
+print len(result)
+
+answer =[n for n in xrange(len(result)) if result.find('/company/', n) == n]
+
+
+print result[answer[0]+9:answer[0]+40]
+
+stop = datetime.datetime.now()
+#print when the soft started and ended
+print str(start)
+print str(stop)
